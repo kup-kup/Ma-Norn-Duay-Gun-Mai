9afe96a79d82ed06cd9299c88b0858cbf396135a
kup <konka.um@gmail.com>
2023-10-30 14:19:34 +0700

    Fix title text alingnment

--- Ma-Norn-Duay-Gun-Mai/Form1.Designer.cs
+++ Ma-Norn-Duay-Gun-Mai/Form1.Designer.cs
@@ -49,10 +49,9 @@
             this.enter_button.BackColor = System.Drawing.Color.NavajoWhite;
             this.enter_button.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.enter_button.ForeColor = System.Drawing.Color.DarkOrange;
-            this.enter_button.Location = new System.Drawing.Point(107, 658);
-            this.enter_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.enter_button.Location = new System.Drawing.Point(80, 535);
             this.enter_button.Name = "enter_button";
-            this.enter_button.Size = new System.Drawing.Size(265, 55);
+            this.enter_button.Size = new System.Drawing.Size(199, 45);
             this.enter_button.TabIndex = 0;
             this.enter_button.Text = "Enter Program";
             this.enter_button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -64,10 +63,9 @@
             this.exit_button.BackColor = System.Drawing.Color.NavajoWhite;
             this.exit_button.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.exit_button.ForeColor = System.Drawing.Color.DarkOrange;
-            this.exit_button.Location = new System.Drawing.Point(456, 658);
-            this.exit_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exit_button.Location = new System.Drawing.Point(342, 535);
             this.exit_button.Name = "exit_button";
-            this.exit_button.Size = new System.Drawing.Size(265, 55);
+            this.exit_button.Size = new System.Drawing.Size(199, 45);
             this.exit_button.TabIndex = 1;
             this.exit_button.Text = "Exit";
             this.exit_button.UseVisualStyleBackColor = false;
@@ -79,10 +77,9 @@
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(202)))));
             this.label1.Font = new System.Drawing.Font("Cooper Black", 28.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Chocolate;
-            this.label1.Location = new System.Drawing.Point(117, 34);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(85, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(581, 54);
+            this.label1.Size = new System.Drawing.Size(472, 44);
             this.label1.TabIndex = 2;
             this.label1.Text = "Ma-Norn-Duay-Gun-Mai";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -93,12 +90,12 @@
             this.subtitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(202)))));
             this.subtitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
             this.subtitle.ForeColor = System.Drawing.Color.Chocolate;
-            this.subtitle.Location = new System.Drawing.Point(120, 97);
-            this.subtitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.subtitle.Location = new System.Drawing.Point(103, 77);
             this.subtitle.Name = "subtitle";
-            this.subtitle.Size = new System.Drawing.Size(550, 31);
+            this.subtitle.Size = new System.Drawing.Size(426, 25);
             this.subtitle.TabIndex = 3;
-            this.subtitle.Text = "มา           นอน              ด้วย              กัน            มั้ย";
+            this.subtitle.Text = "มา         นอน              ด้วย              กัน         มั้ย";
+            this.subtitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.subtitle.Click += new System.EventHandler(this.label2_Click);
             // 
             // label2
@@ -107,10 +104,9 @@
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(202)))));
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
             this.label2.ForeColor = System.Drawing.Color.Peru;
-            this.label2.Location = new System.Drawing.Point(184, 486);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(138, 395);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(450, 31);
+            this.label2.Size = new System.Drawing.Size(354, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "นายกนกนก อำนาจยุติธรรม 6642003026";
             this.label2.Click += new System.EventHandler(this.label2_Click_1);
@@ -121,10 +117,9 @@
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(202)))));
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
             this.label3.ForeColor = System.Drawing.Color.Peru;
-            this.label3.Location = new System.Drawing.Point(204, 530);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(153, 431);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(410, 31);
+            this.label3.Size = new System.Drawing.Size(325, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "นางสาวศุภากร ธโนปจัย 6642107626";
             // 
@@ -138,8 +133,7 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(202)))));
             this.pictureBox2.Image = global::Ma_Norn_Duay_Gun_Mai.Properties.Resources.title_holder_bland;
-            this.pictureBox2.Location = new System.Drawing.Point(87, 34);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(65, 27);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(502, 81);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -150,8 +144,7 @@
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(241)))), ((int)(((byte)(202)))));
             this.pictureBox3.Image = global::Ma_Norn_Duay_Gun_Mai.Properties.Resources.name_holder_bland;
-            this.pictureBox3.Location = new System.Drawing.Point(179, 479);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Location = new System.Drawing.Point(134, 389);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(364, 71);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -162,7 +155,6 @@
             // 
             this.pictureBox1.Image = global::Ma_Norn_Duay_Gun_Mai.Properties.Resources.room_pre_blur;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1178, 755);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -172,9 +164,9 @@
             // 
             // Title
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(845, 814);
+            this.ClientSize = new System.Drawing.Size(634, 661);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.subtitle);
@@ -184,7 +176,6 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Title";
             this.Text = "Ma-Norn-Duay-Gun-Mai";
             this.Load += new System.EventHandler(this.Title_Load);
